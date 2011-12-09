46c566cbb23686d8245403d1fbda1b600c2a6417
Philip Schuchardt <philipschuchardt@europa.local>
2011-12-09 11:26:24 -0500

    Turned off cavern output

--- trunk/src/cwCavernTask.cpp
+++ trunk/src/cwCavernTask.cpp
@@ -97,7 +97,7 @@ void cwCavernTask::privateSetSurvexFile(QString survexFile) {
   \brief Called when cavern has finished
   */
 void cwCavernTask::cavernFinished(int /*exitCode*/, QProcess::ExitStatus /*exitStatus*/) {
-    qDebug() << "Cavern has finish, outputfile:" << output3dFileName();
-    qDebug() << CavernProcess->readAllStandardOutput();
+//    qDebug() << "Cavern has finish, outputfile:" << output3dFileName();
+//    qDebug() << CavernProcess->readAllStandardOutput();
     done();
 }
