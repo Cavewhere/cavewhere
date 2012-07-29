de690460701011c593b21bcf57ca567b96470c07
Philip Schuchardt <vpicaver@gmail.com>
2012-07-29 15:21:00 -0400

    Fixed Ticket #192 - Image Resolution isn't being save / loaded correctly

--- src/cwSerialization.h
+++ src/cwSerialization.h
@@ -313,6 +313,10 @@ namespace boost {
         archive >> BOOST_SERIALIZATION_NVP(rotation);
         archive >> BOOST_SERIALIZATION_NVP(scraps);
 
+        note.setImage(image);
+        note.setRotate(rotation);
+        note.setScraps(scraps);
+
         //Added support for resolution
         if(version >= 2) {
             cwImageResolution* imageResolution;
@@ -320,11 +324,6 @@ namespace boost {
             *(note.imageResolution()) = *imageResolution; //Copy the image resolution into the note
             delete imageResolution; //Delete the loaded image resolution
         }
-
-        note.setImage(image);
-        note.setRotate(rotation);
-        note.setScraps(scraps);
-
     }
 
     ///////////////////////////// cwImage ///////////////////////////////
