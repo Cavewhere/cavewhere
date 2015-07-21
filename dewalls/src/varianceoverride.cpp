415d114cf0f7488c528aebdda71de295accc4146
Andy Edwards <jedwards@fastmail.com>
2015-07-21 01:02:16 -0500

    I guess I don't know exactly how to remove things from the index...

--- dewalls/src/varianceoverride.cpp
+++
@@ -1,9 +0,0 @@
-#include "varianceoverride.h"
-
-namespace dewalls {
-
-const QSharedPointer<VarianceOverride> VarianceOverride::FLOATED(new FloatedVarianceOverride());
-const QSharedPointer<VarianceOverride> VarianceOverride::FLOATED_TRAVERSE(new FloatedTraverseVarianceOverride());
-
-} // namespace dewalls
-
