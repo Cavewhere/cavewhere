ee5665103efe2d12ce63ed043be36009ec5602b4
Andy Edwards <jedwards@fastmail.com>
2015-08-24 23:28:35 -0500

    fixed Compass survey team import to use , or ; as delimiter

--- src/cwCompassImporter.cpp
+++ src/cwCompassImporter.cpp
@@ -307,7 +307,7 @@ void cwCompassImporter::parseSurveyTeam(QFile *file)
     }
 
     QString surveyTeam = file->readLine();
-    surveyTeam = surveyTeamLabel.trimmed();
+    surveyTeam = surveyTeam.trimmed();
 
     LineCount++;
     if(!isFileGood(file, "survey team")) { return; }
@@ -319,7 +319,14 @@ void cwCompassImporter::parseSurveyTeam(QFile *file)
         surveyTeam.resize(100);
     }
 
-    QStringList teamList = surveyTeam.split(" ", QString::SkipEmptyParts);
+    QRegExp delimiter;
+    if (surveyTeam.contains(';')) {
+        delimiter = QRegExp("\\s*;\\s*");
+    } else {
+        delimiter = QRegExp("\\s\\s+|\\s*,\\s*");
+    }
+
+    QStringList teamList = surveyTeam.split(delimiter, QString::SkipEmptyParts);
     if(!teamList.isEmpty()) {
         cwTeam* team = new cwTeam();
 
