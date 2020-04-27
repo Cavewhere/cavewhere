e9e9cc467b3dda5a8f967cdf898666a55436fc77
Philip Schuchardt <vpicaver@gmail.com>
2020-04-27 00:58:16 -0400

    Fixed test_cwRegionIOTask testcase

--- testcases/test_cwRegionIOTask.cpp
+++ testcases/test_cwRegionIOTask.cpp
@@ -5,9 +5,6 @@
 #include "cwRegionIOTask.h"
 
 TEST_CASE("Proto version should report a CaveWhere version correctly", "[cwRegionIOTask]") {
-    bool okay;
     //If this fails you need to update cwRegionIOTask to have the current version in there
-    double versionNumber = cwRegionIOTask::toVersion(cwRegionIOTask::protoVersion()).toDouble(&okay);
-    CHECK(versionNumber > 0);
-    CHECK(okay == true);
+    CHECK(cwRegionIOTask::toVersion(cwRegionIOTask::protoVersion()).isEmpty() == false);
 }
