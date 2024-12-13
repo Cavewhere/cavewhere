636457920be72f462966e37af11014f58e35049b
Philip Schuchardt <vpicaver@gmail.com>
2024-12-13 11:40:23 -0800

    Debugging windows conan cmake

--- conanfile.py
+++ conanfile.py
@@ -14,20 +14,16 @@ class CaveWhereConan(ConanFile):
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-    ("catch2/[>=2.13.10]"),
-    ("protobuf/5.27.0"),
-#    ("survex/1.2.44@cave-software/dev"),
-#    ("dewalls/7e97092a144f153cb9ed7d318808208e9b35c74f@cave-software/dev"),
-#   ("sqlite3/[>=3.42.0]"}),
-    ("libsquish/[>=1.15]"),
+    # ("catch2/[>=2.13.10]"),
+    # ("protobuf/5.27.0"),
+    # ("libsquish/[>=1.15]"),
 
     #We handle survex dependancies here for now, since we're using conan
-    ("wxwidgets/[>=3.2.5]"),
-    ("glew/[>=2.2.0]"),
-    ("proj/9.3.1"),
-    # ("zlib/[>=1.2.13]"),
-    ("libtiff/[>=4.5.1]"),
-    ("gdal/3.8.3"),
+    # ("wxwidgets/[>=3.2.5]"),
+    # ("glew/[>=2.2.0]"),
+    # ("proj/9.3.1"),
+    # ("libtiff/[>=4.5.1]"),
+    # ("gdal/3.8.3"),
     ]
 
     options = {"system_qt": [True, False]}
@@ -35,42 +31,33 @@ class CaveWhereConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv", "VirtualRunEnv"
 
     def requirements(self):
-        self.requires("expat/2.6.2", override=True)
-        #self.requires("sqlite3/3.46.1", override=True)
-        self.requires("libpng/1.6.44", override=True)
+        # self.requires("expat/2.6.2", override=True)
+        # self.requires("libpng/1.6.44", override=True)
 
-        # Or add a new requirement!
-        if not self.options.system_qt:
-            self.requires("qt/6.7.3")
-            self.requires("xkbcommon/1.6.0", override=True)
-            self.requires("sqlite3/3.45.0", override=True)
+        # # Or add a new requirement!
+        # if not self.options.system_qt:
+        #     self.requires("qt/6.7.3")
+        #     self.requires("xkbcommon/1.6.0", override=True)
+        #     self.requires("sqlite3/3.45.0", override=True)
 
     def configure(self):
-        if not self.options.system_qt:
-            self.options["qt"].shared = True
-            self.options["qt"].qtshadertools = True
-            self.options["qt"].qtdeclarative = True
-            self.options["qt"].qtsvg = True
-            self.options["qt"].qttools = True
-            self.options["qt"].qttranslations = True
-            self.options["qt"].qtimageformats = True
+        # if not self.options.system_qt:
+        #     self.options["qt"].shared = True
+        #     self.options["qt"].qtshadertools = True
+        #     self.options["qt"].qtdeclarative = True
+        #     self.options["qt"].qtsvg = True
+        #     self.options["qt"].qttools = True
+        #     self.options["qt"].qttranslations = True
+        #     self.options["qt"].qtimageformats = True
 
-        #Arrow fails on github linux build, disable
-        self.options["gdal"].with_arrow = False
+        # #Arrow fails on github linux build, disable
+        # self.options["gdal"].with_arrow = False
 
-        #This prevents protoc from needing zlib which adds a failing rpath protoc
-        self.options["protobuf"].with_zlib=False
+        # #This prevents protoc from needing zlib which adds a failing rpath protoc
+        # self.options["protobuf"].with_zlib=False
 
-        #This prevents xcode build from failing
-        self.options["libtiff"].zstd=False
-        # self.options["gdal"].shared=True
-
-        #This is survex dependancy
-        #self.options["wxwidgets"].webview=False
-        # self.options["wxwidgets"].shared=True
-#        self.options["tiff"].shared=True
-#        self.options["proj"].with_tiff=False
-        # self.options["zlib"].shared=True
+        # #This prevents xcode build from failing
+        # self.options["libtiff"].zstd=False
 
         self._check_perl_locale_po()
 
