8f08eb20a33f2732613f932bf527bd5ac7d580ac
Philip Schuchardt <vpicaver@gmail.com>
2025-02-03 21:48:41 -0800

    Removed DXT1 compression test

--- testcases/test_cwImageCompressionUpdater.cpp
+++ testcases/test_cwImageCompressionUpdater.cpp
@@ -15,8 +15,6 @@ TEST_CASE("cwRootData should automatically update compression for notes", "[cwIm
     auto rootData = std::make_unique<cwRootData>();
     auto project = rootData->project();
 
-    // REQUIRE(cwOpenGLSettings::instance()->useDXT1Compression());
-
     auto checkNotesAreGood = [&rootData, project]() {
         //Test that the cwNotes have mipmaps
         QList<cwNote*> notes = rootData->regionTreeModel()->all<cwNote*>(QModelIndex(), &cwRegionTreeModel::note);
@@ -29,44 +27,17 @@ TEST_CASE("cwRootData should automatically update compression for notes", "[cwIm
         }
     };
 
-    // auto checkNotesAreBad = [&rootData, project]() {
-    //     //Test that the cwNotes have mipmaps
-    //     QList<cwNote*> notes = rootData->regionTreeModel()->all<cwNote*>(QModelIndex(), &cwRegionTreeModel::note);
-
-    //     CHECK(notes.size() == 3);
-
-    //     cwImageDatabase imageDatabase(project->filename());
-    //     for(cwNote* note : notes) {
-    //         QList<int> mipmaps = note->image().mipmaps();
-    //         CHECK(mipmaps.size() == 0);
-    //     }
-    // };
-
     SECTION("Update on load") {
         fileToProject(project, "://datasets/test_cwImageCompressionUpdater/NotesNeedUpdating.cw");
         rootData->futureManagerModel()->waitForFinished();
         checkNotesAreGood();
     }
-
-    // SECTION("Update on dxt1 compression changed") {
-    //     // cwOpenGLSettings::instance()->setUseDXT1Compression(false);
-    //     fileToProject(project, "://datasets/test_cwImageCompressionUpdater/NotesNeedUpdating.cw");
-
-    //     checkNotesAreBad();
-
-    //     // cwOpenGLSettings::instance()->setUseDXT1Compression(true);
-    //     rootData->futureManagerModel()->waitForFinished();
-
-    //     checkNotesAreGood();
-    // }
 }
 
 TEST_CASE("cwRootData should automatically update compression for scaps", "[cwImageCompressionUpdater]") {
 
     auto rootData = std::make_unique<cwRootData>();
     auto project = rootData->project();
-
-    // REQUIRE(cwOpenGLSettings::instance()->useDXT1Compression());
 
     auto checkScrapsAreGood = [&rootData, project]() {
         //Test that the cwNotes have mipmaps
@@ -77,26 +48,8 @@ TEST_CASE("cwRootData should automatically update compression for scaps", "[cwIm
         cwImageDatabase imageDatabase(project->filename());
         for(cwScrap* scrap : scraps) {
             CHECK(imageDatabase.imageExists(scrap->triangulationData().croppedImage().original()));
-            // QList<int> mipmaps = scrap->triangulationData().croppedImage().mipmaps();
-            // CHECK(mipmaps.size() > 0);
-            // for(int id : mipmaps) {
-            //     CHECK(imageDatabase.imageExists(id));
-            // }
         }
     };
-
-    // auto checkScrapsAreBad = [&rootData, project]() {
-    //     //Test that the cwNotes have mipmaps
-    //     QList<cwScrap*> scraps = rootData->regionTreeModel()->all<cwScrap*>(QModelIndex(), &cwRegionTreeModel::scrap);
-
-    //     CHECK(scraps.size() == 1);
-
-    //     cwImageDatabase imageDatabase(project->filename());
-    //     for(cwScrap* scrap : scraps) {
-    //         QList<int> mipmaps = scrap->triangulationData().croppedImage().or();
-    //         CHECK(mipmaps.size() == 0);
-    //     }
-    // };
 
     SECTION("Update on load") {
         fileToProject(project, "://datasets/test_cwImageCompressionUpdater/ScrapsNeedUpdating.cw");
@@ -104,18 +57,5 @@ TEST_CASE("cwRootData should automatically update compression for scaps", "[cwIm
         rootData->taskManagerModel()->waitForTasks();
         checkScrapsAreGood();
     }
-
-    // SECTION("Update on dxt1 compression changed") {
-    //     // cwOpenGLSettings::instance()->setUseDXT1Compression(false);
-    //     fileToProject(project, "://datasets/test_cwImageCompressionUpdater/ScrapsNeedUpdating.cw");
-
-    //     checkScrapsAreBad();
-
-    //     // cwOpenGLSettings::instance()->setUseDXT1Compression(true);
-    //     rootData->futureManagerModel()->waitForFinished();
-    //     rootData->taskManagerModel()->waitForTasks();
-
-    //     checkScrapsAreGood();
-    // }
 }
 
