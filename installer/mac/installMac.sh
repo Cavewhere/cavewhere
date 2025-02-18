f2cc85552e01e6af37fde100e6c0cb8618205817
Philip Schuchardt <vpicaver@gmail.com>
2025-02-18 12:39:27 -0800

    Improve macos installer script

--- installer/mac/installMac.sh
+++ installer/mac/installMac.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
 # Ensure all arguments are provided
-if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" || -z "$5" ]]; then
+if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
     echo "Error: Missing arguments."
-    echo "Usage: $0 <version> <macdeployqt_path> <email> <code_sign_id> <teamid>"
+    echo "Usage: $0 <macdeployqt_path> <email> <code_sign_id> <teamid>"
     echo "Current code sign id's:"
     security find-identity -v -p codesigning
     exit 1
 fi
 
+version=`git describe`
+macdeployqt_path=$1
+email=$2
+codesignid=$3
+team=$4
+
 security find-identity -v -p codesigning
-echo "Version:" $1
-echo "macdeployqt:" $2
-echo "email:" $3
-echo "code sign id:" $4
-echo "team:" $5
+echo "Version:" $version
+echo "macdeployqt:" $macdeployqt_path
+echo "email:" $email
+echo "code sign id:" $codesignid
+echo "team:" $team
 
 
-$2 CaveWhere.app -qmldir=cavewherelib #-sign-for-notarization=$3
+$macdeployqt_path CaveWhere.app -qmldir=cavewherelib #-sign-for-notarization=$email
 
 #Code sign the bundle
-find CaveWhere.app/Contents/Plugins -type f -name "*.dylib" -exec codesign --force --options runtime --deep --sign $4 {} \;
-codesign --force --deep --options runtime --verbose --sign $4 CaveWhere.app
-#codesign --force --deep --options runtime --verbose --sign $4 CaveWhere.app/Contents/MacOS/CaveWhere
+find CaveWhere.app/Contents/Plugins -type f -name "*.dylib" -exec codesign --force --options runtime --deep --sign $codesignid {} \;
+codesign --force --deep --options runtime --verbose --sign $codesignid CaveWhere.app
+#codesign --force --deep --options runtime --verbose --sign $codesignid CaveWhere.app/Contents/MacOS/CaveWhere
 
 #codesign --verify --deep --strict --verbose=2 CaveWhere.app
 
 #Notrize the bundle
 ditto -c -k --keepParent CaveWhere.app CaveWhere.zip
-echo xcrun notarytool submit CaveWhere.zip --apple-id "$3" --team-id "$5"
-xcrun notarytool submit CaveWhere.zip --apple-id "$3" --team-id "$5"
+echo xcrun notarytool submit CaveWhere.zip --apple-id "$email" --team-id "$team"
+xcrun notarytool submit CaveWhere.zip --apple-id "$email" --team-id "$team"
 
 echo ""
 echo "Check the submitting history with this:"
-echo xcrun notarytool history --apple-id "$3" --team-id "$5"
+echo xcrun notarytool history --apple-id "$email" --team-id "$team" "| head"
 
 echo ""
 echo "Check the log with this:"
-echo xcrun notarytool log "<id>" --apple-id "$3" --team-id "$5"
+echo xcrun notarytool log "<id>" --apple-id "$email" --team-id "$team"
 
 echo ""
 echo "After being accepted run staple"
@@ -55,7 +61,7 @@ echo create-dmg \
     --background ../../installer/mac/dmgBackground.png \
     --icon "Cavewhere.app" 170 175 \
     --no-internet-enable \
-    '"CaveWhere $1.dmg"' \
+    \""CaveWhere $version.dmg"\" \
     "CaveWhere.app"
 
 echo ""
