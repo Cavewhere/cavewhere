808b4b94e96f00af46169696604b11b9ecc32452
Philip Schuchardt <vpicaver@gmail.com>
2025-02-27 15:07:34 -0800

    Enable the wayland option in qt

--- conanfile.py
+++ conanfile.py
@@ -52,6 +52,7 @@ class CaveWhereConan(ConanFile):
             self.options["qt"].qttools = True
             self.options["qt"].qttranslations = True
             self.options["qt"].qtimageformats = True
+            self.options["qt"].qtwayland = True
 
         #Arrow fails on github linux build, disable
         self.options["gdal"].with_arrow = False
