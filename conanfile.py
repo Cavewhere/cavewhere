27bbaa4d5898cd36a0c6c90fb76ecd51502b70e6
Philip Schuchardt <vpicaver@gmail.com>
2025-05-19 17:29:03 -0500

    Updated conan dependancies
    - Also updated README.md with conan bug workaround

--- conanfile.py
+++ conanfile.py
@@ -14,15 +14,14 @@ class CaveWhereConan(ConanFile):
     version = "2025.2"
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-    ("catch2/[>=2.13.10]"),
-    ("protobuf/5.27.0"),
+    ("catch2/[>=3.7.1]"),
+    ("protobuf/[>=5.27.0]"),
 
     #We handle survex dependancies here for now, since we're using conan
     ("wxwidgets/[>=3.2.6]"),
     ("glew/[>=2.2.0]"),
-    ("proj/9.3.1"),
     ("libtiff/[>=4.5.1]"),
-    ("gdal/3.8.3"),
+    ("gdal/[>=3.5.3]"),
     ]
 
     options = {"system_qt": [True, False]}
@@ -30,18 +29,20 @@ class CaveWhereConan(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv", "VirtualRunEnv"
 
     def requirements(self):
-        self.requires("expat/2.6.2", override=True)
-        self.requires("libpng/1.6.44", override=True)
-        self.requires("sqlite3/3.45.0") #override=True breaks the include dir for sqlite3 don't use it
+        self.requires("expat/[>=2.6.2]", override=True)
+        self.requires("libpng/[>=1.6.44]", override=True)
+        self.requires("proj/[>=9.3.1]")
 
         # Or add a new requirement!
         if not self.options.system_qt:
-            self.requires("qt/6.7.3")
-            self.requires("xkbcommon/1.6.0", override=True)
+            self.requires("qt/[>=6.7.3]")
+            self.requires("xkbcommon/[>=1.6.0]", override=True)
+            self.requires("sqlite3/[>=3.45.0]", override=True)
+        else:
+            self.requires("sqlite3/[>=3.44.2]")
 
     def build_requirements(self):
         self.tool_requires("protobuf/5.27.0")
-#        self.tool_requires("cmake/[>=3 <4]")
 
     def configure(self):
         if not self.options.system_qt:
