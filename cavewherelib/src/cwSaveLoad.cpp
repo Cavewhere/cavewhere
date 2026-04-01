Cavewhere/cavewhere

commit 1c47c159710bbcd0c5c9aadc035c9ef5c552ddfa

Add per-entity version checks and surface load warnings to UI
  Each entity file (cwproj, cwcave, cwtrip, cwnote, cwnote3d) is now
  checked against the current proto version during loadAll. If an entity
  was written by a newer CaveWhere, a warning is collected instead of
  silently skipping or hard-rejecting the load. The project-level version
  check in loadProject was changed from a fatal error to a warning so
  projects always load best-effort.

  Warnings are stored in ProjectLoadData::errors, persisted via
  cwSaveLoad::lastLoadErrors(), and appended to cwProject::errorModel()
  so the user sees: "Saving is disabled because it would lose data added
  by the newer version. Please upgrade CaveWhere to save or sync this
  project."

  loadAll() now calls loadMessage() inline for caves, trips, and
  notes instead of delegating to loadCave()/loadTrip() etc., enabling
  version checks and error collection at each level. Fatal parse errors
  are also collected instead of being silently dropped.

  Tests: two Catch2 test cases with six sections covering version
  warnings for cwproj/cwcave/cwtrip, multiple warnings, and cwProject
  errorModel surfacing.

  See #357. Save/sync blocking planned in #361.

cavewherelib/src/cwSaveLoad.cpp
@@ -28,6 +28,7 @@
 #include "cwUniqueConnectionChecker.h"
 #include "cwGlobals.h"
 #include "cwSyncMergeRegistry.h"
+#include "cwError.h"
 
 //Async future
 #include <asyncfuture.h>
@@ -1434,6 +1435,7 @@ struct cwSaveLoad::Data {
     RemoteApplyGuardState remoteApplyGuard;
     bool pendingIdentityRepairSave = false;
     std::optional<cwSaveLoad::SyncReport> lastSyncReport;
+    QList<cwError> lastLoadErrors;
 
     static QList<cwSurveyChunkData> fromProtoSurveyChunks(const google::protobuf::RepeatedPtrField<CavewhereProto::SurveyChunk> & protoList);
 
@@ -2709,6 +2711,7 @@ QFuture<ResultBase> cwSaveLoad::loadImpl(const QString &filename)
                                                             const auto& loadData = projectDataFuture.result().value();
                                                             d->projectMetadata = loadData.metadata;
                                                             d->pendingIdentityRepairSave = loadData.identityRepair.required;
+                                                            d->lastLoadErrors = loadData.errors;
                                                             emit dataRootChanged();
                                                             d->m_regionTreeModel->cavingRegion()->setData(loadData.region);
 
@@ -3768,6 +3771,28 @@ QFuture<ResultString> cwSaveLoad::saveAllFromV6(
         .future();
 }
 
+template<typename ProtoType>
+static std::optional<cwError> checkEntityVersion(const ProtoType& proto, const QString& filename)
+{
+    if (!proto.has_fileversion() || !proto.fileversion().has_version()) {
+        return std::nullopt;
+    }
+    const int version = proto.fileversion().version();
+    if (version > cwRegionIOTask::protoVersion()) {
+        return cwError(
+            QStringLiteral("\"%1\" was created by a newer version of CaveWhere (v%2, file version %3). "
+                           "This copy only supports file version %4. "
+                           "Saving is disabled because it would lose data added by the newer version. "
+                           "Please upgrade CaveWhere to save or sync this project.")
+                .arg(QFileInfo(filename).fileName())
+                .arg(cwRegionIOTask::toVersion(version))
+                .arg(version)
+                .arg(cwRegionIOTask::protoVersion()),
+            cwError::Warning);
+    }
+    return std::nullopt;
+}
+
 QFuture<Monad::Result<cwSaveLoad::ProjectLoadData>> cwSaveLoad::loadAll(const QString &filename)
 {
     return cwConcurrent::run([filename]() {
@@ -3799,14 +3824,29 @@ QFuture<Monad::Result<cwSaveLoad::ProjectLoadData>> cwSaveLoad::loadAll(const QS
             std::sort(caveFiles.begin(), caveFiles.end(), filePathLess);
 
             for (const QFileInfo &caveFileInfo : caveFiles) {
-                auto caveResult = loadCave(caveFileInfo.absoluteFilePath());
-                if (caveResult.hasError()) {
-                    // FIXME: log or collect the error
-                    qDebug() << "Cave result has errror:" << caveResult.errorMessage();
+                const QString cavePath = caveFileInfo.absoluteFilePath();
+                auto caveProtoResult = loadMessage<CavewhereProto::Cave>(cavePath);
+                if (caveProtoResult.hasError()) {
+                    loadData.errors.append(cwError(
+                        QStringLiteral("Could not load cave \"%1\": %2")
+                            .arg(caveFileInfo.fileName(), caveProtoResult.errorMessage()),
+                        cwError::Fatal));
                     continue;
                 }
 
-                cwCaveData cave = caveResult.value();
+                const auto& caveProto = caveProtoResult.value();
+                auto caveVersionWarning = checkEntityVersion(caveProto, cavePath);
+                if (caveVersionWarning) {
+                    loadData.errors.append(*caveVersionWarning);
+                }
+
+                cwCaveData cave;
+                if(caveProto.has_name()) {
+                    cave.name = QString::fromStdString(caveProto.name());
+                }
+                if (caveProto.has_id()) {
+                    cave.id = toUuid(caveProto.id());
+                }
 
                 // Load all trips for this cave
                 QDir caveDir = caveFileInfo.absoluteDir();
@@ -3827,19 +3867,30 @@ QFuture<Monad::Result<cwSaveLoad::ProjectLoadData>> cwSaveLoad::loadAll(const QS
                     std::sort(tripFiles.begin(), tripFiles.end(), filePathLess);
 
                     for (const QFileInfo &tripFileInfo : tripFiles) {
-                        auto tripResult = loadTrip(tripFileInfo.absoluteFilePath());
+                        const QString tripPath = tripFileInfo.absoluteFilePath();
+                        auto tripProtoResult = loadMessage<CavewhereProto::Trip>(tripPath);
 
-                        if (tripResult.hasError()) {
-                            // FIXME: log or collect the error
+                        if (tripProtoResult.hasError()) {
+                            loadData.errors.append(cwError(
+                                QStringLiteral("Could not load trip \"%1\": %2")
+                                    .arg(tripFileInfo.fileName(), tripProtoResult.errorMessage()),
+                                cwError::Fatal));
                             continue;
                         }
 
-                        cwTripData trip = tripResult.value();
+                        const auto& tripProto = tripProtoResult.value();
+                        auto tripVersionWarning = checkEntityVersion(tripProto, tripPath);
+                        if (tripVersionWarning) {
+                            loadData.errors.append(*tripVersionWarning);
+                        }
+
+                        cwTripData trip = cwSaveLoad::tripDataFromProtoTrip(tripProto);
 
                         QDir tripDir = tripFileInfo.absoluteDir();
 
-                        auto loadObjectsFromNotesDir = [tripDir, regionDir, &filePathLess](const QString& fileSuffix,
-                                                                                             auto&& loadFunc,
+                        auto loadObjectsFromNotesDir = [tripDir, &filePathLess, &loadData](const QString& fileSuffix,
+                                                                                             auto&& loadProtoFunc,
+                                                                                             auto&& convertFunc,
                                                                                              auto& destinationList)
                         {
                             QDir notesDir = tripDir.filePath("notes");
@@ -3852,27 +3903,42 @@ QFuture<Monad::Result<cwSaveLoad::ProjectLoadData>> cwSaveLoad::loadAll(const QS
                                                                          QDir::Name | QDir::IgnoreCase | QDir::LocaleAware);
                             std::sort(files.begin(), files.end(), filePathLess);
                             for (const QFileInfo& fileInfo : files) {
-                                auto result = loadFunc(fileInfo.absoluteFilePath(), regionDir);
-                                if (result.hasError()) {
-                                    // FIXME: log or collect the error
+                                const QString notePath = fileInfo.absoluteFilePath();
+                                auto protoResult = loadProtoFunc(notePath);
+                                if (protoResult.hasError()) {
+                                    loadData.errors.append(cwError(
+                                        QStringLiteral("Could not load \"%1\": %2")
+                                            .arg(fileInfo.fileName(), protoResult.errorMessage()),
+                                        cwError::Fatal));
                                     continue;
                                 }
 
-                                destinationList.append(result.value());
+                                auto versionWarning = checkEntityVersion(protoResult.value(), notePath);
+                                if (versionWarning) {
+                                    loadData.errors.append(*versionWarning);
+                                }
+
+                                destinationList.append(convertFunc(protoResult.value(), notePath));
                             }
                         };
 
                         //Load 2D notes
                         loadObjectsFromNotesDir(QStringLiteral("cwnote"),
-                                                [](const QString& filename, const QDir& regionDir) {
-                                                    return loadNote(filename, regionDir);
+                                                [](const QString& path) {
+                                                    return loadMessage<CavewhereProto::Note>(path);
+                                                },
+                                                [](const CavewhereProto::Note& proto, const QString& path) {
+                                                    return cwSaveLoad::noteDataFromProtoNote(proto, path);
                                                 },
                                                 trip.noteModel.notes);
 
                         //Load 3D lidar notes
                         loadObjectsFromNotesDir(QStringLiteral("cwnote3d"),
-                                                [](const QString& filename, const QDir& regionDir) {
-                                                    return loadNoteLiDAR(filename, regionDir);
+                                                [](const QString& path) {
+                                                    return loadMessage<CavewhereProto::NoteLiDAR>(path);
+                                                },
+                                                [](const CavewhereProto::NoteLiDAR& proto, const QString& path) {
+                                                    return cwSaveLoad::noteLiDARDataFromProtoNoteLiDAR(proto, path);
                                                 },
                                                 trip.noteLiDARModel.notes);
 
@@ -3906,15 +3972,19 @@ Monad::Result<cwSaveLoad::ProjectLoadData> cwSaveLoad::loadProject(const QString
                                 return Result<ProjectLoadData>(QStringLiteral("Legacy CaveWhere v7 project files are not supported."));
                             }
 
+                            ProjectLoadData loadData;
+
                             if (fileVersion > cwRegionIOTask::protoVersion()) {
-                                return Result<ProjectLoadData>(
-                                    QStringLiteral("Project file version %1 is newer than supported version %2.")
+                                loadData.errors.append(cwError(
+                                    QStringLiteral("This project was created by a newer version of CaveWhere "
+                                                   "(v%1, file version %2). This copy only supports file version %3. "
+                                                   "Saving is disabled because it would lose data added by the newer version. "
+                                                   "Please upgrade CaveWhere to save or sync this project.")
+                                        .arg(cwRegionIOTask::toVersion(fileVersion))
                                         .arg(fileVersion)
                                         .arg(cwRegionIOTask::protoVersion()),
-                                    static_cast<int>(SyncErrorCode::IncompatibleProjectVersion));
+                                    cwError::Warning));
                             }
-
-                            ProjectLoadData loadData;
                             if (projectProto.has_name()) {
                                 loadData.region.name = QString::fromStdString(projectProto.name());
                             }
@@ -6131,6 +6201,11 @@ QFuture<Monad::ResultBase> cwSaveLoad::checkoutAndReconcile(const QString& refSp
 std::optional<cwSaveLoad::SyncReport> cwSaveLoad::lastSyncReport() const
 {
     return d->lastSyncReport;
+}
+
+QList<cwError> cwSaveLoad::lastLoadErrors() const
+{
+    return d->lastLoadErrors;
 }
 
 QFuture<Monad::Result<cwSaveLoad::ReconcileExternalResult>> cwSaveLoad::reconcileExternalImpl(const SyncReport& report,

cavewherelib/src/cwSaveLoad.h
@@ -34,6 +34,7 @@ class cwNoteLiDAR;
 class cwNoteLiDARData;
 #include "cwRemoteAuthProvider.h"
 #include "cwCavingRegionData.h"
+#include "cwError.h"
 #include "cwProjectedProfileScrapViewMatrix.h"
 #include "cwFutureManagerToken.h"
 #include "cwResultDir.h"
@@ -140,6 +141,7 @@ public:
         cwCavingRegionData region;
         ProjectMetadataData metadata;
         IdentityRepairData identityRepair;
+        QList<cwError> errors;
     };
 
     struct SyncReport {
@@ -200,6 +202,8 @@ public:
     QFuture<Monad::ResultString> saveAllFromV6(const QDir& dir, const cwProject* region, const QString& projectFileName);
 
     static QFuture<Monad::Result<ProjectLoadData>> loadAll(const QString& filename);
+
+    QList<cwError> lastLoadErrors() const;
 
     static Monad::Result<ProjectLoadData> loadProject(const QString& filename);
     static Monad::Result<cwTripData> loadTrip(const QString& filename);

cavewherelib/src/cwProject.cpp
@@ -806,6 +806,12 @@ QFuture<ResultBase> cwProject::loadHelperImpl(const QString& filename, LoadParam
                         emit loaded();
                     }
 
+                    // Surface any load warnings/errors (e.g. newer-version entities)
+                    const auto loadErrors = m_saveLoad->lastLoadErrors();
+                    if (!loadErrors.isEmpty()) {
+                        ErrorModel->append(loadErrors);
+                    }
+
                     // Check for missing LFS files; notify the user if any are found.
                     const QDir projectDir = QFileInfo(this->filename()).absoluteDir();
                     auto missingFuture = QQuickGit::GitRepository::hasMissingLfsFiles(projectDir, this);

testcases/test_cwSaveLoad.cpp
@@ -25,6 +25,11 @@
 #include "cwRegionIOTask.h"
 #include "cavewhereVersion.h"
 #include "ProjectFilenameTestHelper.h"
+#include "cwError.h"
+#include "cwErrorListModel.h"
+#include "cwFutureManagerModel.h"
+#include "LoadProjectHelper.h"
+#include <asyncfuture.h>
 
 //QQuickGit includes
 #include "GitRepository.h"
@@ -42,6 +47,8 @@
 #include <QDate>
 #include <QHash>
 #include <QTemporaryDir>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 #include <set>
 
@@ -1899,4 +1906,199 @@ TEST_CASE("enqueueFlushAndCommit creates a new commit on each save() call", "[cw
 
     repo->checkStatus();
     CHECK(repo->modifiedFileCount() == 0);
+}
+
+namespace {
+bool bumpFileVersion(const QString& filePath, int newVersion)
+{
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly)) {
+        return false;
+    }
+    QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
+    file.close();
+
+    if (!doc.isObject()) {
+        return false;
+    }
+
+    QJsonObject root = doc.object();
+    QJsonObject fileVersion = root[QStringLiteral("fileVersion")].toObject();
+    if (fileVersion.isEmpty()) {
+        return false;
+    }
+    fileVersion[QStringLiteral("version")] = newVersion;
+    root[QStringLiteral("fileVersion")] = fileVersion;
+
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+        return false;
+    }
+    file.write(QJsonDocument(root).toJson());
+    return true;
+}
+
+QStringList findEntityFiles(const QString& rootPath, const QStringList& suffixes)
+{
+    QStringList result;
+    QDirIterator it(rootPath, suffixes, QDir::Files, QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        result.append(it.next());
+    }
+    result.sort();
+    return result;
+}
+}
+
+TEST_CASE("loadAll collects version warnings for newer entity files", "[cwSaveLoad]") {
+    auto rootData = std::make_unique<cwRootData>();
+    auto project = rootData->project();
+
+    auto region = project->cavingRegion();
+    region->addCave();
+    auto cave = region->cave(0);
+    cave->setName(QStringLiteral("VersionTestCave"));
+    cave->addTrip();
+    auto trip = cave->trip(0);
+    trip->setName(QStringLiteral("VersionTestTrip"));
+
+    QTemporaryDir tempDir;
+    REQUIRE(tempDir.isValid());
+    const QString projectPath = QDir(tempDir.path()).filePath(QStringLiteral("VersionTest.cwproj"));
+    REQUIRE(project->saveAs(projectPath));
+    rootData->futureManagerModel()->waitForFinished();
+    project->waitSaveToFinish();
+
+    const QString savedProjectFile = project->filename();
+    REQUIRE(QFileInfo::exists(savedProjectFile));
+
+    const int futureVersion = cwRegionIOTask::protoVersion() + 1;
+
+    SECTION("No warnings when all entities at current version") {
+        auto loadFuture = cwSaveLoad::loadAll(savedProjectFile);
+        REQUIRE(AsyncFuture::waitForFinished(loadFuture, 10000));
+        REQUIRE_FALSE(loadFuture.result().hasError());
+        CHECK(loadFuture.result().value().errors.isEmpty());
+    }
+
+    SECTION("Warning when cave file has future version") {
+        auto caveFiles = findEntityFiles(QFileInfo(savedProjectFile).absolutePath(), {QStringLiteral("*.cwcave")});
+        REQUIRE(caveFiles.size() == 1);
+        REQUIRE(bumpFileVersion(caveFiles.first(), futureVersion));
+
+        auto loadFuture = cwSaveLoad::loadAll(savedProjectFile);
+        REQUIRE(AsyncFuture::waitForFinished(loadFuture, 10000));
+        REQUIRE_FALSE(loadFuture.result().hasError());
+
+        const auto& errors = loadFuture.result().value().errors;
+        REQUIRE(errors.size() == 1);
+        CHECK(errors.first().type() == cwError::Warning);
+        CHECK(errors.first().message().contains(QStringLiteral("newer version")));
+
+        // Data should still load (best-effort)
+        CHECK(loadFuture.result().value().region.caves.size() == 1);
+    }
+
+    SECTION("Warning when trip file has future version") {
+        auto tripFiles = findEntityFiles(QFileInfo(savedProjectFile).absolutePath(), {QStringLiteral("*.cwtrip")});
+        REQUIRE(tripFiles.size() == 1);
+        REQUIRE(bumpFileVersion(tripFiles.first(), futureVersion));
+
+        auto loadFuture = cwSaveLoad::loadAll(savedProjectFile);
+        REQUIRE(AsyncFuture::waitForFinished(loadFuture, 10000));
+        REQUIRE_FALSE(loadFuture.result().hasError());
+
+        const auto& errors = loadFuture.result().value().errors;
+        REQUIRE(errors.size() == 1);
+        CHECK(errors.first().type() == cwError::Warning);
+
+        // Data should still load (best-effort)
+        CHECK(loadFuture.result().value().region.caves.size() == 1);
+        CHECK(loadFuture.result().value().region.caves.first().trips.size() == 1);
+    }
+
+    SECTION("Warning when project file has future version") {
+        auto projFiles = findEntityFiles(QFileInfo(savedProjectFile).absolutePath(), {QStringLiteral("*.cwproj")});
+        REQUIRE(projFiles.size() == 1);
+        REQUIRE(bumpFileVersion(projFiles.first(), futureVersion));
+
+        auto loadFuture = cwSaveLoad::loadAll(savedProjectFile);
+        REQUIRE(AsyncFuture::waitForFinished(loadFuture, 10000));
+        REQUIRE_FALSE(loadFuture.result().hasError());
+
+        const auto& errors = loadFuture.result().value().errors;
+        REQUIRE(errors.size() == 1);
+        CHECK(errors.first().type() == cwError::Warning);
+        CHECK(errors.first().message().contains(QStringLiteral("newer version")));
+
+        // Data should still load (best-effort)
+        CHECK(loadFuture.result().value().region.caves.size() == 1);
+    }
+
+    SECTION("Multiple warnings when both cave and trip have future versions") {
+        auto caveFiles = findEntityFiles(QFileInfo(savedProjectFile).absolutePath(), {QStringLiteral("*.cwcave")});
+        auto tripFiles = findEntityFiles(QFileInfo(savedProjectFile).absolutePath(), {QStringLiteral("*.cwtrip")});
+        REQUIRE(caveFiles.size() == 1);
+        REQUIRE(tripFiles.size() == 1);
+        REQUIRE(bumpFileVersion(caveFiles.first(), futureVersion));
+        REQUIRE(bumpFileVersion(tripFiles.first(), futureVersion));
+
+        auto loadFuture = cwSaveLoad::loadAll(savedProjectFile);
+        REQUIRE(AsyncFuture::waitForFinished(loadFuture, 10000));
+        REQUIRE_FALSE(loadFuture.result().hasError());
+
+        const auto& errors = loadFuture.result().value().errors;
+        CHECK(errors.size() == 2);
+        for (const auto& error : errors) {
+            CHECK(error.type() == cwError::Warning);
+        }
+    }
+}
+
+TEST_CASE("cwProject surfaces load errors to errorModel for git projects", "[cwSaveLoad]") {
+    auto rootData = std::make_unique<cwRootData>();
+    auto project = rootData->project();
+
+    auto region = project->cavingRegion();
+    region->addCave();
+    auto cave = region->cave(0);
+    cave->setName(QStringLiteral("ErrorModelCave"));
+    cave->addTrip();
+    cave->trip(0)->setName(QStringLiteral("ErrorModelTrip"));
+
+    QTemporaryDir tempDir;
+    REQUIRE(tempDir.isValid());
+    const QString projectPath = QDir(tempDir.path()).filePath(QStringLiteral("ErrorModelTest.cwproj"));
+    REQUIRE(project->saveAs(projectPath));
+    rootData->futureManagerModel()->waitForFinished();
+    project->waitSaveToFinish();
+
+    const QString savedProjectFile = project->filename();
+    REQUIRE(QFileInfo::exists(savedProjectFile));
+
+    // Bump cave version to future
+    auto caveFiles = findEntityFiles(QFileInfo(savedProjectFile).absolutePath(), {QStringLiteral("*.cwcave")});
+    REQUIRE(caveFiles.size() == 1);
+    const int futureVersion = cwRegionIOTask::protoVersion() + 1;
+    REQUIRE(bumpFileVersion(caveFiles.first(), futureVersion));
+
+    // Reload the project through cwProject (git path)
+    auto reloaded = std::make_unique<cwProject>();
+    addTokenManager(reloaded.get());
+    reloaded->loadOrConvert(savedProjectFile);
+    reloaded->waitLoadToFinish();
+
+    // errorModel should contain the version warning
+    REQUIRE(reloaded->errorModel()->count() >= 1);
+    bool foundVersionWarning = false;
+    for (int i = 0; i < reloaded->errorModel()->count(); ++i) {
+        const auto error = reloaded->errorModel()->at(i);
+        if (error.type() == cwError::Warning && error.message().contains(QStringLiteral("newer version"))) {
+            foundVersionWarning = true;
+            break;
+        }
+    }
+    CHECK(foundVersionWarning);
+
+    // Data should still be loaded (best-effort)
+    CHECK(reloaded->cavingRegion()->caveCount() == 1);
 }

commit c185addd781a183eb32451da0d9f9caa0fa527ed

Fix sync version gate broken by 1c47c159 and use IncompatibleProjectVersion error code
  Commit 1c47c159 changed loadProject to append a cwError::Warning to loadData.errors when the pulled project
  version is too new, instead of returning a hard Result error. The sync path only checks
  pulledProjectResult.hasError(), so the incompatibility was silently ignored and the sync proceeded — leaving the
   test "cwProject sync blocks pulled newer project version" failing.

  Restore the version gate in the sync path by storing fileVersion in ProjectLoadData and checking it explicitly
  after loadProject succeeds. An incompatible pulled version now returns a ResultBase error before the sync report
   is built, so lastSyncReport stays nullopt and completeSyncOperation adds the error as a cwError::Warning to the
   error model.

  Use SyncErrorCode::IncompatibleProjectVersion (already defined) as the error code rather than defaulting to 0.

  The loadProject warning in loadData.errors is retained as-is for the open-file path where a softer warning is
  appropriate.

cavewherelib/src/cwSaveLoad.cpp
@@ -3973,6 +3973,7 @@ Monad::Result<cwSaveLoad::ProjectLoadData> cwSaveLoad::loadProject(const QString
                             }
 
                             ProjectLoadData loadData;
+                            loadData.fileVersion = fileVersion;
 
                             if (fileVersion > cwRegionIOTask::protoVersion()) {
                                 loadData.errors.append(cwError(
@@ -5933,6 +5934,16 @@ QFuture<Monad::ResultBase> cwSaveLoad::sync()
                         if (pulledProjectResult.hasError()) {
                             return AsyncFuture::completed(ResultBase(pulledProjectResult.errorMessage(),
                                                                      pulledProjectResult.errorCode()));
+                        }
+
+                        const int pulledVersion = pulledProjectResult.value().fileVersion;
+                        const int supportedVersion = cwRegionIOTask::protoVersion();
+                        if (pulledVersion > supportedVersion) {
+                            return AsyncFuture::completed(ResultBase(
+                                QStringLiteral("Project file version %1 is newer than supported version %2.")
+                                    .arg(pulledVersion)
+                                    .arg(supportedVersion),
+                                static_cast<int>(SyncErrorCode::IncompatibleProjectVersion)));
                         }
 
                         const auto afterHeadResult = QQuickGit::GitRepository::headCommitOid(repoPath);

cavewherelib/src/cwSaveLoad.h
@@ -142,6 +142,7 @@ public:
         ProjectMetadataData metadata;
         IdentityRepairData identityRepair;
         QList<cwError> errors;
+        int fileVersion = 0;
     };
 
     struct SyncReport {